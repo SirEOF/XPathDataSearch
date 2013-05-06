00a418feb28149e6e580d41281d0cc7494e96c0c
Adam Hughes <arhughes@gmail.com>
2013-05-06 15:19:47 -0400

    Add simple data search.

---
+++ XPathDataSearch/MainForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace XPathDataSearch
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xpath_textbox = new System.Windows.Forms.TextBox();
+            this.query_button = new System.Windows.Forms.Button();
+            this.result_grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columns_grid = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.file_textbox = new System.Windows.Forms.TextBox();
+            this.browse_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.namespace_grid = new System.Windows.Forms.DataGridView();
+            this.save_button = new System.Windows.Forms.Button();
+            this.load_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.result_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columns_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.namespace_grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xpath_textbox
+            // 
+            this.xpath_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.xpath_textbox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xpath_textbox.Location = new System.Drawing.Point(12, 160);
+            this.xpath_textbox.Name = "xpath_textbox";
+            this.xpath_textbox.Size = new System.Drawing.Size(677, 20);
+            this.xpath_textbox.TabIndex = 0;
+            // 
+            // query_button
+            // 
+            this.query_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.query_button.Location = new System.Drawing.Point(12, 321);
+            this.query_button.Name = "query_button";
+            this.query_button.Size = new System.Drawing.Size(75, 26);
+            this.query_button.TabIndex = 1;
+            this.query_button.Text = "Run";
+            this.query_button.UseVisualStyleBackColor = true;
+            this.query_button.Click += new System.EventHandler(this.query_button_Click);
+            // 
+            // result_grid
+            // 
+            this.result_grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.result_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.result_grid.Location = new System.Drawing.Point(12, 353);
+            this.result_grid.Name = "result_grid";
+            this.result_grid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_grid.Size = new System.Drawing.Size(677, 397);
+            this.result_grid.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select:";
+            // 
+            // columns_grid
+            // 
+            this.columns_grid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.columns_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.columns_grid.Location = new System.Drawing.Point(12, 199);
+            this.columns_grid.Name = "columns_grid";
+            this.columns_grid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.columns_grid.Size = new System.Drawing.Size(677, 116);
+            this.columns_grid.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Columns:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "File:";
+            // 
+            // file_textbox
+            // 
+            this.file_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.file_textbox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.file_textbox.Location = new System.Drawing.Point(44, 16);
+            this.file_textbox.Name = "file_textbox";
+            this.file_textbox.Size = new System.Drawing.Size(564, 22);
+            this.file_textbox.TabIndex = 6;
+            // 
+            // browse_button
+            // 
+            this.browse_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browse_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse_button.Location = new System.Drawing.Point(614, 12);
+            this.browse_button.Name = "browse_button";
+            this.browse_button.Size = new System.Drawing.Size(75, 26);
+            this.browse_button.TabIndex = 8;
+            this.browse_button.Text = "Browse...";
+            this.browse_button.UseVisualStyleBackColor = true;
+            this.browse_button.Click += new System.EventHandler(this.browse_button_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Namespaces:";
+            // 
+            // namespace_grid
+            // 
+            this.namespace_grid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.namespace_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.namespace_grid.Location = new System.Drawing.Point(12, 57);
+            this.namespace_grid.Name = "namespace_grid";
+            this.namespace_grid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namespace_grid.Size = new System.Drawing.Size(677, 84);
+            this.namespace_grid.TabIndex = 9;
+            // 
+            // save_button
+            // 
+            this.save_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_button.Location = new System.Drawing.Point(93, 321);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(75, 26);
+            this.save_button.TabIndex = 11;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // load_button
+            // 
+            this.load_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.load_button.Location = new System.Drawing.Point(174, 321);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(75, 26);
+            this.load_button.TabIndex = 12;
+            this.load_button.Text = "Load";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.load_button_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 762);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.namespace_grid);
+            this.Controls.Add(this.browse_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.file_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.columns_grid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result_grid);
+            this.Controls.Add(this.query_button);
+            this.Controls.Add(this.xpath_textbox);
+            this.Name = "MainForm";
+            this.Text = "XPath Data Search";
+            ((System.ComponentModel.ISupportInitialize)(this.result_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columns_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.namespace_grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xpath_textbox;
+        private System.Windows.Forms.Button query_button;
+        private System.Windows.Forms.DataGridView result_grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView columns_grid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox file_textbox;
+        private System.Windows.Forms.Button browse_button;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView namespace_grid;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Button load_button;
+    }
+}
+
---
+++ XPathDataSearch/MainForm.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XPathDataSearch
+{
+    public partial class MainForm : Form
+    {
+        DataTable m_namespaces;
+        DataTable m_columns;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            m_namespaces = new DataTable();
+            m_namespaces.Columns.Add("prefix", typeof(string));
+            m_namespaces.Columns.Add("ns", typeof(string));
+
+            namespace_grid.Columns.Add("prefix", "Prefix");
+            namespace_grid.Columns["prefix"].DataPropertyName = "prefix";
+            namespace_grid.Columns.Add("ns", "Namespace");
+            namespace_grid.Columns["ns"].DataPropertyName = "ns";
+            namespace_grid.Columns["ns"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            namespace_grid.AutoGenerateColumns = false;
+            namespace_grid.DataSource = m_namespaces;
+
+            m_columns = new DataTable();
+            m_columns.Columns.Add("name", typeof(string));
+            m_columns.Columns.Add("xpath", typeof(string));
+
+            columns_grid.Columns.Add("name", "Name");
+            columns_grid.Columns["name"].DataPropertyName = "name";
+            columns_grid.Columns.Add("xpath", "XPath");
+            columns_grid.Columns["xpath"].DataPropertyName = "xpath";
+            columns_grid.Columns["xpath"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            columns_grid.AutoGenerateColumns = false;
+            columns_grid.DataSource = m_columns;
+
+            InitializeGrid(namespace_grid);
+            InitializeGrid(columns_grid);
+            InitializeGrid(result_grid, true);
+        }
+
+        private void InitializeGrid(DataGridView grid, bool read_only = false)
+        {
+            grid.BorderStyle = BorderStyle.Fixed3D;
+            grid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Raised;
+            grid.DefaultCellStyle.BackColor = SystemColors.Window;
+            grid.BackgroundColor = SystemColors.Window;
+            grid.ReadOnly = read_only;
+            grid.AllowUserToAddRows = !read_only;
+        }
+
+        private void query_button_Click(object sender, EventArgs e)
+        {
+
+            m_namespaces.AcceptChanges();
+            m_columns.AcceptChanges();
+
+            DataTable data = new DataTable();
+
+            try
+            {
+                var xpath_query = xpath_textbox.Text;
+                var doc = new XPathDocument(file_textbox.Text);
+                var nav = doc.CreateNavigator();
+
+                var manager = new XmlNamespaceManager(nav.NameTable);
+
+                foreach (DataRow ns_row in m_namespaces.Rows)
+                {
+                    var prefix = (string)ns_row["prefix"];
+                    var ns = (string)ns_row["ns"];
+                    manager.AddNamespace(prefix, ns);
+                }
+
+                var iter = nav.Select(xpath_query, manager);
+
+                var columns = new List<Tuple<DataColumn, XPathExpression>>();
+
+                foreach (DataRow column_row in m_columns.Rows)
+                {
+                    var name = (string)column_row["name"];
+                    var query = (string)column_row["xpath"];
+
+                    try
+                    {
+                        var expression = nav.Compile(query);
+                        expression.SetContext(manager);
+
+                        columns.Add(Tuple.Create(data.Columns.Add(name, typeof(string)), expression));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error compiling expression '" + name + "':\n\n" + ex.ToString());
+                        return;
+                    }
+                }
+
+                while (iter.MoveNext())
+                {
+                    var row = data.NewRow();
+
+                    foreach (var column in columns)
+                    {
+                        var result = iter.Current.Evaluate(column.Item2);
+                        row[column.Item1] = result.ToString();
+                    }
+
+                    data.Rows.Add(row);
+                }
+
+                result_grid.DataSource = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void browse_button_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+
+                if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                    file_textbox.Text = ofd.FileName;
+            }
+
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "XPath Data Seach File (*.xpds)|*.xpds";
+
+                if (sfd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    using (var s = sfd.OpenFile())
+                        SaveXml(s);
+                }
+
+            }
+        }
+
+        private void load_button_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "XPath Data Seach File (*.xpds)|*.xpds";
+
+                if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    using (var s = ofd.OpenFile())
+                        LoadXml(s);
+                }
+            }
+
+        }
+
+        void LoadXml(Stream stream)
+        {
+            string xpath_query;
+            var namespaces = new DataTable();
+            var columns = new DataTable();
+
+            namespaces.Columns.Add("prefix", typeof(string));
+            namespaces.Columns.Add("ns", typeof(string));
+
+            columns.Columns.Add("name", typeof(string));
+            columns.Columns.Add("xpath", typeof(string));
+            using (var reader = XmlReader.Create(stream))
+            {
+                reader.ReadStartElement("XPathDataSearch");
+
+                while (reader.IsStartElement("Namespace"))
+                {
+                    var prefix = reader.GetAttribute("prefix");
+                    var ns = reader.ReadElementContentAsString();
+                    namespaces.LoadDataRow(new object[] { prefix, ns }, true);
+                }
+
+                xpath_query = reader.ReadElementContentAsString("Select", "");
+
+                while (reader.IsStartElement("Column"))
+                {
+                    var name = reader.GetAttribute("name");
+                    var value = reader.ReadElementContentAsString();
+
+                    columns.LoadDataRow(new object[] { name, value }, true);
+                }
+            }
+
+            m_namespaces = namespaces;
+            m_columns = columns;
+
+            namespace_grid.DataSource = m_namespaces;
+            columns_grid.DataSource = m_columns;
+            xpath_textbox.Text = xpath_query;
+            result_grid.DataSource = null;
+        }
+
+        void SaveXml(Stream stream)
+        {
+            using (var writer = XmlWriter.Create(stream))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("XPathDataSearch");
+
+                foreach (DataRow ns_row in m_namespaces.Rows)
+                {
+                    writer.WriteStartElement("Namespace");
+                    writer.WriteAttributeString("prefix", (string)ns_row["prefix"]);
+                    writer.WriteString((string)ns_row["ns"]);
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteElementString("Select", xpath_textbox.Text);
+
+                foreach (DataRow column_row in m_columns.Rows)
+                {
+                    writer.WriteStartElement("Column");
+                    writer.WriteAttributeString("name", (string)column_row["name"]);
+                    writer.WriteString((string)column_row["xpath"]);
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+    }
+}
